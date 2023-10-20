01814c55bda8278cf9f0ff75f47db28b5df85185
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-20 10:27:47 +0700

    pop dan shift
    pop dan shift

---
+++ Tugas12.js
@@ -0,0 +1,10 @@
+function Tugas12() {
+    var buahan = ["Pisang", "Jeruk", "Apel", "Mangga"];
+
+    console.log(buahan);
+    var buahan2 = buahan.pop();
+    var buahan3 = buahan.shift();
+    console.log(buahan2+" "+buahan3);
+    return buahan;
+}
+console.log(Tugas12());
